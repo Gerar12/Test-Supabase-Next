8fa835e7ca3afc7299fa3014d260645a3b940427
Gerar <gerar@duck.com>
2024-04-08 01:29:48 -0600

    save

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,22 @@
 "use client";
-
 import Link from "next/link";
 import { useContext, useEffect, useState } from "react";
-
 import { Button } from "@/components/ui/button";
 import AuthModalContext from "@/context/AuthModalContext";
 import CreateProfileContext from "@/context/CreateProfileContext";
 import { supabaseBrowserClient } from "@/utils/supabaseClient";
-import { Session, User } from "@supabase/supabase-js";
-
+import { User } from "@supabase/supabase-js";
 const Navbar = () => {
   const { toggleAuthModal } = useContext(AuthModalContext);
   const { toggleCreateProfileModal } = useContext(CreateProfileContext);
 
   const [user, setUser] = useState<User>();
-  const [isMounted, setisMounted] = useState(false);
+  const [isMounted, setisMounted] = useState<boolean>(false);
 
-  const handleSingnout = async () => {};
-
+  const handleSingnout = async () => {
+    const { error } = await supabaseBrowserClient.auth.signOut();
+    if (!error) setUser(undefined);
+  };
   useEffect(() => {
     const getUserData = async () => {
       const {
@@ -33,7 +32,7 @@ const Navbar = () => {
   }, []);
 
   if (!isMounted) return null;
-
+  console.log(user);
   return (
     <nav className="bg-white border-gray-200">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -45,19 +44,28 @@ const Navbar = () => {
         </Link>
 
         <div className="flex items-center space-x-5 w-auto">
-          <Link
-            href="/profile"
-            className="block text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700"
-          >
-            Profile
-          </Link>
+          {user && (
+            <>
+              <Link
+                href="/profile"
+                className="block text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700"
+              >
+                Profile
+              </Link>
 
-          <Button onClick={toggleCreateProfileModal} variant="outline">
-            Update Profile
-          </Button>
-          <Button onClick={toggleAuthModal} variant="destructive">
-            Auth
-          </Button>
+              <Button onClick={toggleCreateProfileModal} variant="outline">
+                Update Profile
+              </Button>
+              <Button onClick={handleSingnout} variant="destructive">
+                Sign Out
+              </Button>
+            </>
+          )}
+          {!user && (
+            <Button onClick={toggleAuthModal} variant="destructive">
+              Auth
+            </Button>
+          )}
         </div>
       </div>
     </nav>
